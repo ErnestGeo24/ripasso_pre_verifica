1eb7885d47e4f57de4a3c7524031f9805b8bb012
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-01-15 10:20:13 +0000

    punto3

--- ripasso_verifica/src/app/app.module.ts
+++ ripasso_verifica/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ConteinerComponent } from './conteiner/conteiner.component';
+import { MacchinaComponent } from './macchina/macchina.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConteinerComponent
+    ConteinerComponent,
+    MacchinaComponent
   ],
   imports: [
     BrowserModule
