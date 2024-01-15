35880d1ab5d05c507d4226504faebd138528b01f
ErnestGeo24 <ernest1.avasi@gmail.com>
2024-01-15 10:18:03 +0000

    punto2

--- ripasso_verifica/src/app/conteiner/conteiner.component.ts
+++ ripasso_verifica/src/app/conteiner/conteiner.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './conteiner.component.html',
   styleUrls: ['./conteiner.component.css']
 })
-export class ConteinerComponent {
-
+export class UserListComponent {
+  names: string[]; 
+  constructor() { 
+     this.names = ['Toyota Corolla','Audi Q5', 'Ford Mustang', ' Honda Civic', 'Volkswagen Golf','Chevrolet Camaro','Mercedes-Benz E-Class',' BMW 3 Series','Tesla Model S','Jeep Wrangler'];
+  }
 }
